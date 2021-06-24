8557d7479553ef38e504d9e716a00fbe8a9db73c
tahabinaziz <thzz9574@gmail.com>
2021-06-25 03:42:36 +0500

    ok

--- client/web/quizapp/src/Constant/constant.js
+++ client/web/quizapp/src/Constant/constant.js
@@ -1,17 +1,3 @@
 export const MCQ = ["A", "B", "C", "D"];
 export const TF = ["T", "F"];
 export const BLANK = ["Blank"];
-export const optArr = [
-    {
-      value: "t/f",
-      option: ["T", "F"],
-    },
-    {
-      value: "mcqs",
-      option: ["", "", "", ""],
-    },
-    {
-      value: "blank",
-      option: [""],
-    },
-  ];
